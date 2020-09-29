8cc115a8f908eb7e8df89e1381f98104b8d672ac
eeddy <eeddy@unb.ca>
2020-09-29 19:25:05 -0300

    Test

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,6 @@ mod tests {
 
     #[test]
     fn test() {
-        assert_eq!(hello_world(), "Hello Worlds".to_string());
+        assert_eq!(hello_world(), "Hello Worldss".to_string());
     }
 }
