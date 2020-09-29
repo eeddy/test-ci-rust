9c7c0e7624cbb4c773975f631c6314d17142a447
eeddy <eeddy@unb.ca>
2020-09-29 19:22:20 -0300

    Hello World App

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    println!("{}", hello_world());
+}
+
+fn hello_world() -> String {
+    return "Hello World".to_string();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(hello_world(), "Hello Worlds".to_string());
+    }
+}
